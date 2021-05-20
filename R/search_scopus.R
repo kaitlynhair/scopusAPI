c81fd71a261d53e8b8a35532d06052580888eb84
Kaitlyn Hair <kaitlyn.hair@ed.ac.uk>
2021-05-20 10:23:17 +0100

    Update documentation

--- R/search_scopus.R
+++ R/search_scopus.R
@@ -1,3 +1,15 @@
+#' Run a search query to the Scopus API
+#'
+#' @param api_key API key from developers portal
+#' @param string query text
+#' @param retMax max records to return
+
+#' @return data as df
+
+#' @export
+#' @import httr
+#' @import XML
+
 search_scopus <- function(api_key, string, retMax){
   
   theData <- searchByString(string=string, api_key=api_key, retMax=retMax, outfile="scopus.xml") 
